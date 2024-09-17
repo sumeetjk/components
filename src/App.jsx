dcd6aa3673bdc3ad8c8190fa956ae5005219a6cc
sumeet kumawat <sumeetjkumawat@gmail.com>
2024-09-17 18:36:55 +0530

    implement accordion component

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Accordion from "./components/Accordion";
 export default function App() {
   const items = [
     {
+      id: "asdfadsf",
       label: "can i use react on a project?",
       content:
         "you can use react on any of your project you can use react on any of your project you can use react on any of your project you can use react on any of your project",
     },
     {
+      id: "kjoualkrtj",
       label: "can i use javascript on a project?",
       content:
         "you can use javascript on any of your project you can use javascript on any of your project you can use javascript on any of your project you can use javascript on any of your project",
     },
     {
+      id: "iuqewt",
       label: "can i use css on a project?",
       content:
         "you can use css on any of your project you can use css on any of your project you can use css on any of your project you can use css on any of your project",
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,43 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 export default function Accordion({ items }) {
-  return (
-    <div>
-      <h1>Accordion!!</h1>
-    </div>
-  );
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    console.log("stale version of expandedIndex", expandedIndex);
+    setExpandedIndex((currentExpandedIndex) => {
+      console.log("up to date versio of expanded Index", currentExpandedIndex);
+      if (currentExpandedIndex === nextIndex) {
+        return -1;
+      } else {
+        return nextIndex;
+      }
+    });
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+
+    return (
+      <div key={item.id}>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    );
+  });
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
