98a0c5c8e3c8f120b0da77f9ed3f1a1e72087b69
sumeet kumawat <sumeetjkumawat@gmail.com>
2024-09-18 23:15:51 +0530

    add styling to the dropdown using tailwind css

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
 export default function Dropdown({ options, onChange, value }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,16 +16,30 @@ export default function Dropdown({ options, onChange, value }) {
 
   const renderedItems = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
-        {option.value}
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
       </div>
     );
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>{value?.label || "Select..."}</div>
-      {isOpen && <div>{renderedItems}</div>}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absoulute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedItems}
+        </div>
+      )}
     </div>
   );
 }
