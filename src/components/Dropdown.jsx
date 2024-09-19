04165e7a67688e86c3de1e1efadecb1a8c43d1eb
sumeet kumawat <sumeetjkumawat@gmail.com>
2024-09-19 16:27:45 +0530

    add complete functionality of dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,10 +1,29 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 export default function Dropdown({ options, onChange, value }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handler, true);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
@@ -28,7 +47,7 @@ export default function Dropdown({ options, onChange, value }) {
   });
 
   return (
-    <div className="w-48 relative">
+    <div ref={divEl} className="w-48 relative">
       <Panel
         className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
