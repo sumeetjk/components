8e8ca5f208e85f50478d163958ef4e4c547ae750
sumeet kumawat <sumeetjkumawat@gmail.com>
2024-09-22 23:05:11 +0530

    create modal the easy but wrong way

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,8 @@
 export default function Modal() {
-  return <div>Modal</div>;
+  return (
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 bg-white p-10">Modal in the middle</div>
+    </div>
+  );
 }
